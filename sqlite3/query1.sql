61843aaa19868b93ca8789f1f1577c7dc22b1c68
luculliano <vederecento@gmail.com>
2023-03-28 23:37:00 +0500

    initial commit

---
+++ sqlite3/query1.sql
@@ -0,0 +1,37 @@
+/*                     table creation queries                     */
+create table department (
+  department_id integer primary key autoincrement,  /* 'int' not allowed */
+  department_name varchar(50)
+);
+
+create table employees (
+  employees_id integer primary key autoincrement,
+  first_name varchar(50),
+  last_name varchar(50),
+  department_id int not null,
+  foreign key (department_id) references department (department_id)
+);
+
+
+/*                     inserts queries                     */
+insert into department (department_name)
+  values ('prod'), ('commerce'), ('govern');
+
+insert into employees (first_name, last_name, department_id)
+  values ('Nick', 'Love', 1), ('Sam', 'Rock', 2),
+  ('Llama', 'Howard', 1), ('Nine', 'Aone', 1), ('Rok', 'Joma', 4);
+
+
+/*                     joins                     */
+
+/* inner join берет поля из в обеих таблиц, значения которых совпадают. */
+/* Если пары не нашлось для записей, он их не выведет. */
+SELECT first_name, last_name, department_name FROM employees
+  JOIN department USING (department_id)
+  ORDER BY first_name ASC;  /* asc as default order */
+
+/* left join отличается тем что если для второй таблицы пары не нашлось - добавит null */
+/* right join соответственно наоборот */
+SELECT first_name, last_name, department_name FROM employees
+  LEFT JOIN department USING (department_id)
+  ORDER BY first_name ASC;  /* asc as default order */
