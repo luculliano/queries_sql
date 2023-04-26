c740fdbe998b45d014a5b81d849659ab2dac2faf
luculliano <vederecento@gmail.com>
2023-04-26 17:57:13 +0500

    add regexp.png and query2

---
+++ sqlite3/query2.sql
@@ -0,0 +1,12 @@
+/*присвоение номеров значениям как вычисляемое поле*/
+/*f CAST(col as type) для преоьразования типа, чтобы в рез-те не округлялось до целого,
+поэтому одно поле надо к плавающему числу привести*/
+SELECT tg_uid, tries, memes,
+    (SELECT count(*) FROM users t1 WHERE t1.memes <= t2.memes) n,
+    ROUND(CAST(memes AS REAL) / tries * 100, 2) perc FROM
+    users t2 ORDER BY memes;
+
+/*присвоение номеров значениям как вычисляемое поле в порядке вычислемого поля*/
+/*f ROW_NUMBER исп для присвоение порядкого номера значению в заданном порядке*/
+SELECT *, ROW_NUMBER() OVER (ORDER BY perc, memes DESC) as row_number FROM
+    (SELECT tg_uid, tries, memes, ROUND(CAST(memes AS REAL) / tries * 100, 2) perc FROM users);
